ffc690812202f59dfaf85069aa48134ac33060ea
erickTS <ericktorres4045.alumnos.udg.mx>
2021-03-26 16:08:41 -0600

    Set up of the project.
    Added all files and some code to set up the project using bootstrap.

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+        <link rel="stylesheet" type="text/css" href="css/bootstrap-theme.min.css">
+        <title>TITULO</title>
+    </head>
+    <main>
+        <header>
+        </header>
+        <body>
+
+
+            <script src="js/bootstrap.min.js"></script>
+            <script src="js/jquery.js"></script>
+        </body>
+        <footer>
+        </footer>
+    </main>
+</html>
